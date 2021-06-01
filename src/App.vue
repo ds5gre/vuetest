6e475277dada90bf5fd3a83ad22d25f954a4b733
play.js <git@gituser.com>
2021-06-01 19:36:15 +0900

    no

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <ul>
+    <li v-for="hero in dcHeros">
+    <h1>{{ hero.name }}</h1>
+    </li>
+    </ul>
+    <input v-model:value="newHero" placeholder="input some text"/>
+    <h3>{{ newHero }}</h3>
   </div>
 </template>
 
@@ -12,6 +19,17 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  
+  data(){
+    return{
+      newHero:"",
+      dcHeros: [
+        {"name":"Wonder Woman"},
+        {"name":"Arrow"},
+        {"name":"Super Man"}
+      ],
+    };
   }
 }
 </script>
